1e0bc64ad23a938d7981f777a3054b4478c8ffe5
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2014-06-16 17:35:02 -0700

    Push update to 1.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.0',
+    version='1.0.1',
 
     description='Official reference Imgur python library with OAuth2',
     long_description='',
